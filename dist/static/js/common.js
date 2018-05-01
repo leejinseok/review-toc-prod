1868c0e3113f1b4efe84af2dc5ab24e1ab583043
이진석 <sonaky47@gmail.com>
2018-05-01 22:17:08 +0900

    login nav dropdown menu

--- dist/static/js/common.js
+++ dist/static/js/common.js
@@ -8,6 +8,7 @@ $(document).ready(function () {
     }
   });
   handleOptions(); // handle options
+  handleAccordionLogined();
 });
 
 function showPrivatePolicy () {
@@ -160,4 +161,51 @@ function myPageThumbnailChange (file) {
 function handleNavMenuActive (index) {
   var list = $('nav.main ul.left li');
   list.eq(index).addClass('active');
+}
+
+
+/**
+ * handle accordion (CPA, 체험단 ...)
+ * @return {void}
+ */
+function handleAccordionLogined (accordionIndex, itemIndex) {
+  var accordions = $('nav.main.logined .accordion');
+  var parentParagraph = $('nav.main.logined .panel p');
+  var items = $('nav.main.logined .panel p a');
+  var downArrowSrc = {
+    'selected': './static/images/mypage/my_profile_up_selected.png',
+    'unselected': './static/images/mypage/my_profile_down.png'
+  };
+
+  accordions.click(function () {
+    var panel = $(this).next();
+    var downArrow = $(this).find('img');
+    var maxHeight = parseInt(panel.css('max-height'));
+    var scollHeight = panel.prop('scrollHeight');
+    accordionIndex = $(accordions).index(this);
+    if (maxHeight) {
+      panel.css('max-height', 0);
+      downArrow.attr('src', downArrowSrc.unselected);
+      // accordions.removeClass('active');
+      return;
+    }
+    accordions.removeClass('active');
+    accordions.eq(accordionIndex).addClass('active');
+    // downArrow.attr('src', downArrowSrc.selected);
+    panel.css('max-height', scollHeight);
+  });
+
+  // accordions
+  //   .eq(accordionIndex)
+  //   .addClass('active')
+  //   .click();
+
+  items.click(function (e) {
+    var parentPanel = $(this).parent().parent();
+    parentPanel.find('a').removeClass('active');
+    $(this).addClass('active');
+  });
+
+  // items.eq(itemIndex).click();
+  // parentParagraph.eq(itemIndex).addClass('active');
 }
