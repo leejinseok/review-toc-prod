bf6886ee4d006f3d477ad562c03ae4c60eeceaf1
이진석 <sonaky47@gmail.com>
2018-05-03 00:44:43 +0900

    ppt 21

--- dist/static/js/service_guide.js
+++ dist/static/js/service_guide.js
@@ -19,6 +19,7 @@ function handleSwiper () {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
+    loop: true,
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
