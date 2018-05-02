6c2b9fb45209cae7f974715227af164383e6bc8c
이진석 <sonaky47@gmail.com>
2018-05-02 21:45:29 +0900

    ppt 17

--- dist/static/js/signup.js
+++ dist/static/js/signup.js
@@ -1,7 +1,36 @@
 $(document).ready(function () {
   handleSignUpTypeButton();
   handleSignupTypePartnerOrAdvertizer();
+  handleExtraInfo();
 });
+
+function handleExtraInfo () {
+  var btn = $('.sub-info .btn-container button');
+  var bodyCpa = $('.sub-info ul.sub-info-wrap.cpa');
+  var bodyTester = $('.sub-info ul.sub-info-wrap.tester');
+  var src = {
+    uncheked: '/static/images/sign-up/uncheked.png',
+    checked: '/static/images/sign-up/cheked.png'
+  };
+
+  btn.click(function () {
+    var type = $(this).data('type');
+    btn.removeClass('selected');
+    btn.find('img').attr('src', src.uncheked);
+    $(this).addClass('selected');
+    $(this).find('img').attr('src', src.checked);
+    
+    if (type === 'cpa') {
+      bodyTester.removeClass('active');
+      bodyCpa.addClass('active');
+    } 
+
+    if (type === 'tester') {
+      bodyCpa.removeClass('active');
+      bodyTester.addClass('active');
+    }
+  });
+}
 
 function handleSignupTypePartnerOrAdvertizer () {
   var btn = $('section.sign-up .btn-container .signup-type');
