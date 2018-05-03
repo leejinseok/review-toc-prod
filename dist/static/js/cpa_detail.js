2c840afba406a602506d5bc968676ff415a38b7b
이진석 <sonaky47@gmail.com>
2018-05-03 12:33:32 +0900

    ppt 32

--- dist/static/js/cpa_detail.js
+++ dist/static/js/cpa_detail.js
@@ -1,4 +1,18 @@
 $(document).ready(function () {
   handleNavMenuActive(1);
   handleBodyClick();
+  handleScrollToEachTab();
 });
+
+/**
+ * 탭 클릭하였을때 스크롤 핸들
+ */
+function handleScrollToEachTab () {
+  var a = $('section.detail .main .with-tab nav.tab ul li a');
+  a.click(function () {
+    var to = $(this).data('to');
+    var of = $('#' + to).offset();
+    var top = of.top - 93 - 10;
+    $(window).scrollTop(top);
+  });
+}
