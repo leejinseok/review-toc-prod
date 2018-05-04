20266dd6199c73d3c8409867ed451a27395a6ae6
이진석 <sonaky47@gmail.com>
2018-05-04 11:21:43 +0900

    ppt 50

--- dist/static/js/common.js
+++ dist/static/js/common.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
   });
   handleOptions(); // handle options
   handleAccordionLogined();
-  handleNavMainFixed(); 
+  handleNavMainFixed();
   handleSearch();
 });
 
@@ -230,4 +230,21 @@ function handleAccordionLogined (accordionIndex, itemIndex) {
 
   // items.eq(itemIndex).click();
   // parentParagraph.eq(itemIndex).addClass('active');
+}
+
+function parse_query_string (query) {
+  var vars = query.split("&");
+  var query_string = {};
+  for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split("=");
+    if (typeof query_string[pair[0]] === "undefined") {
+      query_string[pair[0]] = decodeURIComponent(pair[1]);
+    } else if (typeof query_string[pair[0]] === "string") {
+      var arr = [query_string[pair[0]], decodeURIComponent(pair[1])];
+      query_string[pair[0]] = arr;
+    } else {
+      query_string[pair[0]].push(decodeURIComponent(pair[1]));
+    }
+  }
+  return query_string;
 }
---
+++ dist/static/js/logined.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  handleLetterExist();
+});
+
+function handleLetterExist () {
+  alert('hi');
+  var search = window.location.search;
+  console.log(search);
+}
