18f6afd6eff9d3d2699789d51ccca525068e6e32
이진석 <sonaky47@gmail.com>
2018-05-12 23:07:24 +0900

    update

--- dist/static/js/common.js
+++ dist/static/js/common.js
@@ -14,7 +14,68 @@ $(document).ready(function () {
   handleLetterClick();
   handleLetterIcon();
   handleBodyClickForModalRoot();
+  handleAccordionSlideMenu();
 });
+
+/**
+ * handle accordion (CPA, 체험단 ...)
+ * @return {void}
+ */
+function handleAccordionSlideMenu (accordionIndex, itemIndex) {
+  var accordions = $('.wrapper-sidemenu .accordion');
+  var parentParagraph = $('.wrapper-sidemenu .panel p');
+  var items = $('.wrapper-sidemenu .panel p a');
+  var downArrowSrc = {
+    'selected': './static/images/mypage/my_profile_up_black.png',
+    'unselected': './static/images/mypage/my_profile_down.png'
+  };
+
+  accordions.click(function () {
+    var panel = $(this).next('.panel');
+    var downArrow = $(this).find('img');
+    var maxHeight = parseInt(panel.css('max-height'));
+    var scollHeight = panel.prop('scrollHeight');
+    accordionIndex = $(accordions).index(this);
+    if (maxHeight) {
+      panel.css('max-height', 0);
+      downArrow.attr('src', downArrowSrc.unselected);
+      return;
+    }
+
+    if ($(this).hasClass('no-menus')) {
+      accordions.removeClass('active');
+      accordions.eq(accordionIndex).addClass('active');
+    }
+    
+    downArrow.attr('src', downArrowSrc.selected);
+    panel.css('max-height', scollHeight);
+  });
+
+  accordions
+    .eq(accordionIndex)
+    .addClass('active')
+    .click();
+
+  items.click(function (e) {
+    var parentPanel = $(this).parent().parent();
+    parentPanel.find('a').removeClass('active');
+    $(this).addClass('active');
+  });
+
+  items.eq(itemIndex).click();
+  parentParagraph.eq(itemIndex).addClass('active');
+}
+
+function toogleSlideMenu () {
+  var wrapper = $('.wrapper-sidemenu');
+
+  if (wrapper.hasClass('active')) {
+    wrapper.removeClass('active');
+    return;
+  }
+  
+  wrapper.addClass('active');
+}
 
 function handleAllCheckMessageModal (allCheck) {
   var allCheck = $(allCheck);
@@ -53,6 +114,10 @@ function handleBodyClickForModalRoot () {
     var targetId = e.target.id;
     if (targetId === 'modal-messages') {
       hideModal();
+    }
+    
+    if (targetId === 'wrapper-sidemenu') {
+      $('.wrapper-sidemenu').removeClass('active');
     }
   });
 }
