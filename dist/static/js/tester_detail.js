2a51233773c903f82f4457e0c408caa45bea14e6
이진석 <sonaky47@gmail.com>
2018-05-19 01:16:10 +0900

    update

---
+++ dist/static/js/tester_detail.js
@@ -0,0 +1,64 @@
+$(document).ready(function () {
+  handleNavMenuActive(1);
+  handleBodyClick();
+  handleScrollToEachTab();
+  handleRelativeCpa();
+  handleRelativeTester();
+});
+
+function handleRelativeTester () {
+  var list = $('.sub .tester .card');
+  var img = $('.sub .tester .card .wrapper-img');
+
+  list.hover(function () {
+    var index = list.index(this);
+    var curItem = list.eq(index);
+    curItem.find('.dDay-and-recruitment').addClass('active');
+    img.eq(index).addClass('active');
+  });
+
+  list.mouseleave(function () {
+    var index = list.index(this);
+    var curItem = list.eq(index);
+    curItem.find('.dDay-and-recruitment').removeClass('active');
+    img.eq(index).removeClass('active');
+  });
+}
+
+/**
+ * cpa list card hover
+ * @return {void}
+ */
+function handleRelativeCpa () {
+  var cpaList = $('.sub .cpa .card');
+  var img = $('.sub .cpa .card .wrapper-img');
+
+  cpaList.hover(function () {
+    var index = cpaList.index(this);
+    var curItem = cpaList.eq(index);
+
+    curItem.find('.price').addClass('active');
+    img.eq(index).addClass('active');
+  });
+
+  cpaList.mouseleave(function () {
+    var index = cpaList.index(this);
+    var curItem = cpaList.eq(index);
+
+    curItem.find('.price').removeClass('active');
+    img.eq(index).removeClass('active');
+  });
+}
+
+/**
+ * 탭 클릭하였을때 스크롤 핸들
+ */
+function handleScrollToEachTab () {
+  var a = $('section.detail .main .with-tab nav.tab ul li a');
+  a.click(function () {
+    var to = $(this).data('to');
+    var of = $('#' + to).offset();
+    var top = of.top - 93 - 10;
+    $(window).scrollTop(top);
+  });
+}
