60750185c8c65a7cbe75c298dbe01cd1a3674a10
이진석 <sonaky47@gmail.com>
2018-05-19 00:50:19 +0900

    update

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 module.exports = {
-    ejs: {
-        path: {
-            watch: './src/**/*.ejs',
-            exec: [
-                './src/**/*.ejs',
-                '!./src/views/variables/**/*'
-            ]
-        }
-    },
-    scss: {
-        path: './src/static/css/**/*.scss'
+  ejs: {
+    path: {
+      watch: './src/**/*.ejs',
+      exec: [
+        './src/**/*.ejs',
+        '!./src/views/variables/**/*'
+      ]
     }
+  },
+  scss: {
+    path: './src/static/css/**/*.scss'
+  }
 }
